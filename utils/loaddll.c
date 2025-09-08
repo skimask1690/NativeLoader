4fdcbfb1b26b4128904eba7bb8cceddf593ab87b
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-09-07 22:41:38 -0400

    Update loaddll.c

--- utils/loaddll.c
+++ utils/loaddll.c
@@ -1,8 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 // Type for DLL entry point
 typedef BOOL(WINAPI *DllEntryProc)(HINSTANCE, DWORD, LPVOID);
