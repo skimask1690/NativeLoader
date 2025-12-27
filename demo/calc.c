6053b50622e3ef381a5e1033b15ff7a3756c626e
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-27 01:43:30 -0500

    Update calc.c

--- demo/calc.c
+++ demo/calc.c
@@ -1,35 +1,32 @@
 #include "winapi_loader.h"
 
 // -------------------- Function pointer types --------------------
-typedef BOOL (WINAPI *CreateProcessW_t)(
-    LPCWSTR lpApplicationName,
-    LPWSTR lpCommandLine,
+typedef BOOL (WINAPI *CreateProcessA_t)(
+    LPCSTR lpApplicationName,
+    LPSTR lpCommandLine,
     LPSECURITY_ATTRIBUTES lpProcessAttributes,
     LPSECURITY_ATTRIBUTES lpThreadAttributes,
     BOOL bInheritHandles,
     DWORD dwCreationFlags,
     LPVOID lpEnvironment,
-    LPCWSTR lpCurrentDirectory,
-    LPSTARTUPINFOW lpStartupInfo,
+    LPCSTR lpCurrentDirectory,
+    LPSTARTUPINFOA lpStartupInfo,
     LPPROCESS_INFORMATION lpProcessInformation
 );
 
 // -------------------- Strings --------------------
 STRINGA(kernel32_dll, "kernel32.dll")
-STRINGA(createprocessw, "CreateProcessW")
-STRINGW(path, "C:\\Windows\\System32\\calc.exe")
+STRINGA(createprocessa, "CreateProcessA")
+STRINGA(command, "calc.exe")
 
 // -------------------- Entry point --------------------
 __attribute__((section(".text.start")))
 void _start(void) {
     HMODULE hKernel32 = myGetModuleHandleA(kernel32_dll);
-    CreateProcessW_t pCreateProcessW = (CreateProcessW_t)myGetProcAddress(hKernel32, createprocessw);
+    CreateProcessA_t pCreateProcessA = (CreateProcessA_t)myGetProcAddress(hKernel32, createprocessa);
 
-    STARTUPINFOW si;
+    STARTUPINFOA si = { sizeof(si) };
     PROCESS_INFORMATION pi;
-
-    *(STARTUPINFOW*)&si = (STARTUPINFOW){0}; si.cb = sizeof(STARTUPINFOW);
-    *(PROCESS_INFORMATION*)&pi = (PROCESS_INFORMATION){0};
-
-    pCreateProcessW(path, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	
+    pCreateProcessA(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
 }
