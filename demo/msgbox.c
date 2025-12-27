7d982bac2105829f09817f03cf1e8743836f54dc
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-27 01:45:41 -0500

    Update msgbox.c

--- demo/msgbox.c
+++ demo/msgbox.c
@@ -1,18 +1,26 @@
 #include "winapi_loader.h"
 
 // -------------------- Function pointer types --------------------
-typedef int (WINAPI *MessageBoxW_t)(HWND, LPCWSTR, LPCWSTR, UINT);
+typedef int (WINAPI *MessageBoxA_t)(HWND, LPCSTR, LPCSTR, UINT);
+typedef VOID (WINAPI *ExitProcess_t)(UINT uExitCode);
 
 // -------------------- Strings --------------------
 STRINGW(user32_dll, "user32.dll")
-STRINGA(messageboxw, "MessageBoxW")
-STRINGW(hello_msg, "Hello from shellcode!")
-STRINGW(title_msg, "C Shellcode Demo")
+STRINGA(kernel32_dll, "kernel32.dll")
+STRINGA(messageboxa, "MessageBoxA")
+STRINGA(hello_msg, "Hello from shellcode!")
+STRINGA(title_msg, "C Shellcode Demo")
+STRINGA(exitprocess, "ExitProcess")
 
 // -------------------- Entry point --------------------
 __attribute__((section(".text.start")))
 int _start(void) {
     HMODULE hUser32 = myLoadLibraryW(user32_dll);
-    MessageBoxW_t pMessageBoxW = (MessageBoxW_t)myGetProcAddress(hUser32, messageboxw);
-    pMessageBoxW(NULL, hello_msg, title_msg, MB_OK | MB_ICONINFORMATION);
+    HMODULE hKernel32 = myGetModuleHandleA(kernel32_dll);
+
+    MessageBoxA_t pMessageBoxA = (MessageBoxA_t)myGetProcAddress(hUser32, messageboxa);
+    ExitProcess_t pExitProcess = (ExitProcess_t)myGetProcAddress(hKernel32, exitprocess);
+	
+    pMessageBoxA(NULL, hello_msg, title_msg, MB_OK | MB_ICONINFORMATION);
+    pExitProcess(0);
 }
