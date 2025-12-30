f36a572cfcf44a0572319e59eeefa8f0e3200fd2
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-30 02:56:34 -0500

    Update direct_syscall.h

--- syscalls/direct_syscall.h
+++ syscalls/direct_syscall.h
@@ -4,44 +4,14 @@
 #include "winapi_loader.h"
 
 /* ================= Macros ================= */
-#define SYSCALL_PREPARE(fn_name)                                  \
-    void *syscall_ptr = NULL;                                     \
-    void *ntdll_view  = NULL;                                     \
-    do {                                                          \
-        NTDLL_DISK_CTX _ctx = MapNtdllFromDisk();                 \
-        ntdll_view = _ctx.base;                                   \
-        DWORD _ssn = ResolveSSN(&_ctx, fn_name);                  \
-        syscall_ptr = BuildDirectSyscallStub(_ssn);               \
-    } while(0)
+#define SYSCALL_PREPARE(name)                  \
+    do {                                      \
+        NTDLL_DISK_CTX ctx = MapNtdllFromDisk(); \
+        g_ssn  = ResolveSSN(&ctx, name);      \
+        g_stub = BuildDirectSyscallStub(&ctx, g_ssn); \
+    } while (0)
 
-#define SYSCALL_CALL(fn_type, ...)                                       \
-    ({                                                                   \
-        fn_type _fn = (fn_type)syscall_ptr;                              \
-        NTSTATUS _ret = _fn(__VA_ARGS__);                                \
-        if (_fn) {                                                       \
-            NTSTATUS (NTAPI *pNtFreeVirtualMemory)(                      \
-                HANDLE, PVOID *, PSIZE_T, ULONG) =                       \
-                (void *)myGetProcAddress(                                \
-                    myGetModuleHandleA(ntdll_dll), ntfreevm);            \
-                                                                         \
-            PVOID base = _fn;                                            \
-            SIZE_T size = 11;                                            \
-            pNtFreeVirtualMemory((HANDLE)-1, &base, &size, MEM_RELEASE); \
-                                                                         \
-            NTSTATUS (NTAPI *pNtUnmapViewOfSection)(                     \
-                HANDLE, PVOID) =                                         \
-                (void *)myGetProcAddress(                                \
-                    myGetModuleHandleA(ntdll_dll), ntunmapview);         \
-                                                                         \
-            if (ntdll_view) {                                            \
-                pNtUnmapViewOfSection((HANDLE)-1, ntdll_view);           \
-                ntdll_view = NULL;                                       \
-            }                                                            \
-                                                                         \
-            syscall_ptr = NULL;                                          \
-        }                                                                \
-        _ret;                                                            \
-    })
+#define SYSCALL_CALL(type) ((type)g_stub)
 
 /* ================= Strings ================= */
 STRINGA(ntdll_dll, "ntdll.dll");
@@ -53,13 +23,15 @@ STRINGA(ntmapview, "NtMapViewOfSection");
 STRINGA(ntclose, "NtClose");
 STRINGA(ntallocvm, "NtAllocateVirtualMemory");
 STRINGA(ntprotectvm, "NtProtectVirtualMemory");
-STRINGA(ntfreevm, "NtFreeVirtualMemory");
-STRINGA(ntunmapview, "NtUnmapViewOfSection");
+
+/* ================= Globals ================= */
+static DWORD g_ssn;
+static void *g_stub;
 
 /* ================= Types ================= */
 typedef enum _SECTION_INHERIT {
     ViewShare = 1,
-    ViewUnmap = 2 
+    ViewUnmap = 2
 } SECTION_INHERIT;
 
 typedef struct _NTDLL_DISK_CTX {
@@ -74,7 +46,7 @@ static NTDLL_DISK_CTX MapNtdllFromDisk(void) {
     NTSTATUS (NTAPI *NtCreateFile)(PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES, PIO_STATUS_BLOCK, PLARGE_INTEGER, ULONG, ULONG, ULONG, ULONG, PVOID, ULONG);
     NTSTATUS (NTAPI *NtCreateSection)(PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES, PLARGE_INTEGER, ULONG, ULONG, HANDLE);
     NTSTATUS (NTAPI *NtMapViewOfSection)(HANDLE, HANDLE, PVOID *, ULONG_PTR, SIZE_T, PLARGE_INTEGER, PSIZE_T, DWORD, ULONG, ULONG);
-    NTSTATUS (NTAPI *NtClose)(HANDLE);
+	NTSTATUS (NTAPI *NtClose)(HANDLE);
 
     HMODULE ntdll = myGetModuleHandleA(ntdll_dll);
     NtCreateFile = (void *)myGetProcAddress(ntdll, ntcreatefile);
@@ -153,30 +125,25 @@ static DWORD ResolveSSN(NTDLL_DISK_CTX *ctx, const char *name) {
         return *(DWORD *)(f + 4);
 
     for (int i = 0; i < 32; i++)
-        if (f[i] == 0xB8 && f[i + 5] == 0x0F && f[i + 6] == 0x05)
+        if (f[i] == 0xB8 && f[i+5] == 0x0F && f[i+6] == 0x05)
             return *(DWORD *)(f + i + 1);
 
     return 0xFFFFFFFF;
 }
 
-static void *ResolveSyscall(NTDLL_DISK_CTX *ctx, const char *name) {
-    BYTE *f = GetExport(ctx, name);
-
-    for (int i = 0; i < 32; i++)
-        if (f[i] == 0x0F && f[i + 1] == 0x05)
-            return f + i;
-
-    return NULL;
-}
-
 /* ================= Direct syscall stub ================= */
-static void *BuildDirectSyscallStub(DWORD ssn) {
-    NTSTATUS (NTAPI *NtAllocateVirtualMemory)(HANDLE, PVOID *, ULONG_PTR, PSIZE_T, ULONG, ULONG) = (void *)myGetProcAddress(myGetModuleHandleA(ntdll_dll), ntallocvm);
-    NTSTATUS (NTAPI *NtProtectVirtualMemory)(HANDLE, PVOID *, PSIZE_T, ULONG, PULONG) = (void *)myGetProcAddress(myGetModuleHandleA(ntdll_dll), ntprotectvm);
+static void *BuildDirectSyscallStub(NTDLL_DISK_CTX *ctx, DWORD ssn) {
+    NTSTATUS (NTAPI *NtAllocateVirtualMemory)(HANDLE, PVOID *, ULONG_PTR, PSIZE_T, ULONG, ULONG);
+    NTSTATUS (NTAPI *NtProtectVirtualMemory)(HANDLE, PVOID *, PSIZE_T, ULONG, PULONG);
+
+    HMODULE ntdll = myGetModuleHandleA(ntdll_dll);
+    NtAllocateVirtualMemory = (void *)myGetProcAddress(ntdll, ntallocvm);
+    NtProtectVirtualMemory = (void *)myGetProcAddress(ntdll, ntprotectvm);
 
     PVOID base = NULL;
     SIZE_T size = 11;
-    NtAllocateVirtualMemory((HANDLE)-1, &base, 0, &size, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
+
+    NtAllocateVirtualMemory((HANDLE)-1, &base, 0, &size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
     BYTE *p = (BYTE *)base;
     p[0] = 0x4C; p[1] = 0x8B; p[2] = 0xD1; // mov r10, rcx
@@ -184,8 +151,9 @@ static void *BuildDirectSyscallStub(DWORD ssn) {
     p[8] = 0x0F; p[9] = 0x05;               // syscall
     p[10] = 0xC3;                           // ret
 
-    ULONG old;
-    NtProtectVirtualMemory((HANDLE)-1, &base, &size, PAGE_EXECUTE_READ, &old);
+    ULONG oldProt;
+    NtProtectVirtualMemory((HANDLE)-1, &base, &size, PAGE_EXECUTE_READ, &oldProt);
+
     return base;
 }
 
