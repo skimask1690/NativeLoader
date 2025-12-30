4f867b5be3a6394838a2632329e4888d3d5e3a99
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-30 05:02:55 -0500

    Update syscall_example.c

--- syscalls/syscall_example.c
+++ syscalls/syscall_example.c
@@ -5,8 +5,8 @@
 typedef NTSTATUS (NTAPI *NtCreateFile_t)(PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES, PIO_STATUS_BLOCK, PLARGE_INTEGER, ULONG, ULONG, ULONG, ULONG, PVOID, ULONG);
 
 /* ================= Strings ================= */
-STRINGW(filepath, "\\??\\C:\\temp\\test.txt")
 STRINGA(ntcreatefilea, "NtCreateFile");
+STRINGW(filepath, "\\??\\C:\\temp\\test.txt")
 
 /* ================= Entry point ================= */
 __attribute__((section(".text.start")))
@@ -24,6 +24,4 @@ void _start(void) {
     InitializeObjectAttributes(&oa, &us, OBJ_CASE_INSENSITIVE, NULL, NULL);
 
     pNtCreateFile(&hFile, GENERIC_WRITE, &oa, &iosb, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ | FILE_SHARE_WRITE, FILE_OPEN_IF, FILE_NON_DIRECTORY_FILE, NULL, 0);
-	STUB_RELEASE();
-	NTDLL_RELEASE();
 }
