673939bc3a28900619b49739380e4e366d5baac4
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-29 20:15:57 -0500

    Update syscall_example.c

--- syscalls/syscall_example.c
+++ syscalls/syscall_example.c
@@ -1,11 +1,11 @@
 #include "direct_syscall.h"
-// #include "indirect_syscall.h"
+//#include "indirect_syscall.h"
 
 /* ================= Function pointer types ================= */
 typedef NTSTATUS (NTAPI *NtCreateFile_t)(PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES, PIO_STATUS_BLOCK, PLARGE_INTEGER, ULONG, ULONG, ULONG, ULONG, PVOID, ULONG);
 
 /* ================= Strings ================= */
-STRINGW(filepath, "\\??\\C:\\temp\\test.txt")
+STRINGW(filepath, "\\??\\C:\\Users\\robro\\OneDrive\\Desktop\\test.txt")
 STRINGA(ntcreatefilea, "NtCreateFile");
 
 /* ================= Entry point ================= */
@@ -17,13 +17,8 @@ void _start(void) {
     HANDLE hFile = NULL;
     IO_STATUS_BLOCK iosb = {0};
 
-    USHORT len = 0;
-    while (filepath[len]) len++;
-
     UNICODE_STRING us;
-    us.Buffer = filepath;
-    us.Length = len * sizeof(WCHAR);
-    us.MaximumLength = us.Length + sizeof(WCHAR);
+    InitUnicodeString(&us, filepath);
 
     OBJECT_ATTRIBUTES oa;
     InitializeObjectAttributes(&oa, &us, OBJ_CASE_INSENSITIVE, NULL, NULL);
