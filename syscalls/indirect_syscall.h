4ca74ce0b782577921cc1c03721aa49df4541ac2
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-29 21:23:09 -0500

    Update indirect_syscall.h

--- syscalls/indirect_syscall.h
+++ syscalls/indirect_syscall.h
@@ -49,24 +49,41 @@ static NTDLL_DISK_CTX MapNtdllFromDisk(void) {
     NTSTATUS (NTAPI *NtMapViewOfSection)(HANDLE, HANDLE, PVOID *, ULONG_PTR, SIZE_T, PLARGE_INTEGER, PSIZE_T, DWORD, ULONG, ULONG);
 
     HMODULE ntdll = myGetModuleHandleA(ntdll_dll);
-    NtCreateFile    = (void *)myGetProcAddress(ntdll, ntcreatefile);
+    NtCreateFile = (void *)myGetProcAddress(ntdll, ntcreatefile);
     NtCreateSection = (void *)myGetProcAddress(ntdll, ntcreatesection);
     NtMapViewOfSection = (void *)myGetProcAddress(ntdll, ntmapview);
 
     UNICODE_STRING us;
     InitUnicodeString(&us, ntdll_path);
 
-    OBJECT_ATTRIBUTES oa; 
+    OBJECT_ATTRIBUTES oa;
     InitializeObjectAttributes(&oa, &us, OBJ_CASE_INSENSITIVE, NULL, NULL);
 
-    HANDLE hFile; 
+    HANDLE hFile;
     IO_STATUS_BLOCK iosb;
     NtCreateFile(&hFile, FILE_GENERIC_READ, &oa, &iosb, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_OPEN, FILE_NON_DIRECTORY_FILE, NULL, 0);
 
     HANDLE hSection;
     NtCreateSection(&hSection, SECTION_MAP_READ, NULL, NULL, PAGE_READONLY, SEC_IMAGE, hFile);
 
-    NtMapViewOfSection(hSection, (HANDLE)-1, &ctx.base, 0, 0, NULL, &ctx.size, ViewShare, 0, PAGE_READONLY);
+    PVOID base = NULL;
+    SIZE_T size = 0;
+    NtMapViewOfSection(hSection, (HANDLE)-1, &base, 0, 0, NULL, &size, ViewShare, 0, PAGE_READONLY);
+
+    IMAGE_DOS_HEADER *dos = (IMAGE_DOS_HEADER *)base;
+    IMAGE_NT_HEADERS *nt  = (IMAGE_NT_HEADERS *)((BYTE *)base + dos->e_lfanew);
+
+    SIZE_T min_size = nt->OptionalHeader.SizeOfHeaders;
+    for (WORD i = 0; i < nt->FileHeader.NumberOfSections; i++) {
+        IMAGE_SECTION_HEADER *sect = (IMAGE_SECTION_HEADER *)((BYTE *)nt + sizeof(IMAGE_NT_HEADERS) + i * sizeof(IMAGE_SECTION_HEADER));
+        if (sect->Characteristics & IMAGE_SCN_CNT_CODE || sect->VirtualAddress <= nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress) {
+            SIZE_T end = sect->VirtualAddress + sect->Misc.VirtualSize;
+            if (end > min_size) min_size = end;
+        }
+    }
+
+    ctx.base = base;
+    ctx.size = min_size;
 
     return ctx;
 }
