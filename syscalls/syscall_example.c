f8adbb5c1f50d45ae482fa8dd6b22df4e55cdf88
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-30 02:55:52 -0500

    Update syscall_example.c

--- syscalls/syscall_example.c
+++ syscalls/syscall_example.c
@@ -1,16 +1,18 @@
 #include "direct_syscall.h"
+//#include "indirect_syscall.h"
 
 /* ================= Function pointer types ================= */
 typedef NTSTATUS (NTAPI *NtCreateFile_t)(PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES, PIO_STATUS_BLOCK, PLARGE_INTEGER, ULONG, ULONG, ULONG, ULONG, PVOID, ULONG);
 
 /* ================= Strings ================= */
-STRINGW(filepath, "\\??\\C:\\temp\\test.txt")
+STRINGW(filepath, "\\??\\C:\\Users\\robro\\OneDrive\\Desktop\\test.txt")
 STRINGA(ntcreatefilea, "NtCreateFile");
 
 /* ================= Entry point ================= */
 __attribute__((section(".text.start")))
 void _start(void) {
     SYSCALL_PREPARE(ntcreatefilea);
+    NtCreateFile_t pNtCreateFile = SYSCALL_CALL(NtCreateFile_t);
 
     HANDLE hFile = NULL;
     IO_STATUS_BLOCK iosb = {0};
@@ -21,5 +23,5 @@ void _start(void) {
     OBJECT_ATTRIBUTES oa;
     InitializeObjectAttributes(&oa, &us, OBJ_CASE_INSENSITIVE, NULL, NULL);
 
-    SYSCALL_CALL(NtCreateFile_t, &hFile, GENERIC_WRITE, &oa, &iosb, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ | FILE_SHARE_WRITE, FILE_OPEN_IF, FILE_NON_DIRECTORY_FILE, NULL, 0);
+    pNtCreateFile(&hFile, GENERIC_WRITE, &oa, &iosb, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ | FILE_SHARE_WRITE, FILE_OPEN_IF, FILE_NON_DIRECTORY_FILE, NULL, 0);
 }
