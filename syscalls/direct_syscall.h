e02bcf15cf1558c94f47f8a32b4ba9204f5fca05
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-12-31 18:18:25 -0500

    Add NtTerminateProcess support in direct syscall
    Added support for NtTerminateProcess in syscall context.

--- syscalls/direct_syscall.h
+++ syscalls/direct_syscall.h
@@ -15,9 +15,11 @@ STRINGA(ntclose, "NtClose");
 STRINGA(ntallocatevirtualmemory, "NtAllocateVirtualMemory");
 STRINGA(ntprotectvirtualmemory, "NtProtectVirtualMemory");
 STRINGA(ntfreevirtualmemory, "NtFreeVirtualMemory");
+STRINGA(ntterminateprocess, "NtTerminateProcess");
 
 /* ================= Types ================= */
 typedef NTSTATUS (NTAPI *NtUnmapViewOfSection_t)(HANDLE, PVOID);
+typedef NTSTATUS (NTAPI *NtTerminateProcess_t)(HANDLE, NTSTATUS);
 
 typedef struct _NTDLL_DISK_CTX {
     PVOID base;
@@ -69,6 +71,14 @@ DWORD           ResolveSSN(NTDLL_DISK_CTX *ctx, const char *name);
         FreeSyscallStub(ctx, NtUnmapViewOfSection); \
 		DestroySyscallContext(ctx);                 \
     } while(0)
+
+#define SYSCALL_EXIT(ctx, status)                                   \
+    do {                                                            \
+        SYSCALL_PREPARE(ctx, ntterminateprocess);                 \
+        NtTerminateProcess_t NtTerminateProcess =                  \
+            SYSCALL_CALL(ctx, NtTerminateProcess_t);                \
+        NtTerminateProcess((HANDLE)-1, (NTSTATUS)(status));         \
+    } while (0)
 
 /* ================= Implementation ================= */
 SYSCALL_CTX *CreateSyscallContext(void) {
