5c86236a9874b279cd19a48f960405470bfe7300
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2026-01-04 23:45:31 -0500

    Revert changes

--- winapi_loader.h
+++ winapi_loader.h
@@ -1,180 +1,206 @@
-#ifndef WINAPI_LOADER_H
-#define WINAPI_LOADER_H
+#ifndef PE_LOADER_H
+#define PE_LOADER_H
 
-#include <windows.h>
-#include <winternl.h>
+#include "winapi_loader.h"
 
-// -------------------- String macros --------------------
-#define STRINGA(name, value) __attribute__((section(".text"))) static char name[] = value;
-#define STRINGW(name, value) __attribute__((section(".text"))) static wchar_t name[] = L##value;
+// -------------------- Strings --------------------
+STRINGA(ntdll_dll, "ntdll.dll");
+STRINGA(ntallocatevirtualmemory, "NtAllocateVirtualMemory");
+STRINGA(ntprotectvirtualmemory, "NtProtectVirtualMemory");
+STRINGA(ntfreevirtualmemory, "NtFreeVirtualMemory");
 
-// -------------------- XOR helpers (-DXOR) --------------------
-#ifdef XOR
-
-#define XOR_KEY(len) ( \
-    ((len * 59) & 0xFF) ^ \
-    (__DATE__[10] * 37 ^ __DATE__[9] * 31 ^ __DATE__[8] * 29) ^ \
-    (__TIME__[3] * 7 ^ __TIME__[1] * 31 ^ __TIME__[0] * 17 ^ __TIME__[2] * 13) ^ \
-    (__DATE__[7] * 23 ^ __DATE__[5] * 17 ^ __DATE__[6] * 19 ^ __DATE__[4] * 13) ^ \
-    (__TIME__[6] * 29 ^ __TIME__[4] * 23 ^ __TIME__[5] * 19 ^ __TIME__[7] * 11) ^ \
-    (__DATE__[3] * 11 ^ __DATE__[2] * 7 ^ __DATE__[1] * 5 ^ __DATE__[0] * 3) \
-)
-
-static void xor_decode(char* str) {
-    size_t len = 0;
-    while (str[len]) len++;
-    unsigned char key = XOR_KEY(len);
-    for (size_t i = 0; i < len; i++)
-        str[i] ^= key;
-}
-
-#endif // XOR
-
-// -------------------- PEB structs --------------------
-typedef struct _LDR_MODULE {
-  LIST_ENTRY InLoadOrderModuleList;
-  LIST_ENTRY InMemoryOrderModuleList;
-  LIST_ENTRY InInitializationOrderModuleList;
-  PVOID BaseAddress;
-  PVOID EntryPoint;
-  ULONG SizeOfImage;
-  UNICODE_STRING FullDllName;
-  UNICODE_STRING BaseDllName;
-  ULONG Flags;
-  SHORT LoadCount;
-  SHORT TlsIndex;
-  LIST_ENTRY HashTableEntry;
-  ULONG TimeDateStamp;
-} LDR_MODULE, *PLDR_MODULE;
+// -------------------- NTDLL typedefs --------------------
+typedef NTSTATUS(NTAPI* NtAllocateVirtualMemory_t)(HANDLE, PVOID*, ULONG_PTR, PSIZE_T, ULONG, ULONG);
+typedef NTSTATUS(NTAPI* NtProtectVirtualMemory_t)(HANDLE, PVOID*, PSIZE_T, ULONG, PULONG);
+typedef NTSTATUS(NTAPI* NtFreeVirtualMemory_t)(HANDLE, PVOID*, PSIZE_T, ULONG);
 
 // -------------------- Helpers --------------------
-static void AsciiToWideChar(const char* ascii, UNICODE_STRING* ustr, wchar_t* buf, SIZE_T bufCount) {
-    SIZE_T i = 0;
-    while (ascii[i] && i < bufCount - 1) {
-        buf[i] = (wchar_t)ascii[i];
-        i++;
+static ULONG SectionProtection(DWORD characteristics) {
+    if (characteristics & IMAGE_SCN_MEM_EXECUTE)
+        return (characteristics & IMAGE_SCN_MEM_WRITE) ? PAGE_EXECUTE_READWRITE
+               : (characteristics & IMAGE_SCN_MEM_READ) ? PAGE_EXECUTE_READ : PAGE_EXECUTE;
+    else if (characteristics & IMAGE_SCN_MEM_READ)
+        return (characteristics & IMAGE_SCN_MEM_WRITE) ? PAGE_READWRITE : PAGE_READONLY;
+    return PAGE_NOACCESS;
+}
+
+// -------------------- Import/Export resolution --------------------
+static ULONG_PTR ResolveExport(HMODULE mod, const char* name, int isOrdinal) {
+    HMODULE curMod = mod;
+    const char* curName = name;
+    int curIsOrdinal = isOrdinal;
+
+    for (;;) {
+        IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)curMod;
+        IMAGE_NT_HEADERS64* ntE = (IMAGE_NT_HEADERS64*)((BYTE*)curMod + dos->e_lfanew);
+        IMAGE_DATA_DIRECTORY ed = ntE->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+        IMAGE_EXPORT_DIRECTORY* exp = (IMAGE_EXPORT_DIRECTORY*)((BYTE*)curMod + ed.VirtualAddress);
+        DWORD* addrTable = (DWORD*)((BYTE*)curMod + exp->AddressOfFunctions);
+        DWORD* nameTable = (DWORD*)((BYTE*)curMod + exp->AddressOfNames);
+        WORD* ordTable = (WORD*)((BYTE*)curMod + exp->AddressOfNameOrdinals);
+
+        DWORD foundRVA = 0;
+        if (!curIsOrdinal) {
+            for (DWORD i = 0; i < exp->NumberOfNames; i++) {
+                char* en = (char*)((BYTE*)curMod + nameTable[i]);
+                const char* p1 = en;
+                const char* p2 = curName;
+                while (*p1 && *p2 && *p1 == *p2) { p1++; p2++; }
+                if (!*p1 && !*p2) { foundRVA = addrTable[ordTable[i]]; break; }
+            }
+        } else {
+            DWORD ord = (DWORD)((ULONG_PTR)curName & 0xFFFF);
+            foundRVA = addrTable[ord - exp->Base];
+        }
+
+        if (foundRVA >= ed.VirtualAddress && foundRVA < ed.VirtualAddress + ed.Size) {
+            char* fwd = (char*)((BYTE*)curMod + foundRVA);
+            const char* s = fwd;
+
+            size_t dllLen = 0; while (s[dllLen] && s[dllLen] != '.') dllLen++;
+            char* dll = (char*)alloca(dllLen + 1);
+            for (size_t i = 0; i < dllLen; i++) dll[i] = s[i];
+            dll[dllLen] = 0;
+            s += dllLen + 1;
+
+            size_t fnameLen = 0; while (s[fnameLen]) fnameLen++;
+            char* fname = (char*)alloca(fnameLen + 1);
+            for (size_t i = 0; i < fnameLen; i++) fname[i] = s[i];
+            fname[fnameLen] = 0;
+
+            HMODULE fmod = myLoadLibraryA(dll);
+
+            if (fname[0] == '#') {
+                unsigned long ordval = 0; s = fname + 1;
+                while (*s >= '0' && *s <= '9') { ordval = ordval * 10 + (*s - '0'); s++; }
+                curMod = fmod; curName = (const char*)(ordval | IMAGE_ORDINAL_FLAG64); curIsOrdinal = 1;
+            } else {
+                curMod = fmod; curName = fname; curIsOrdinal = 0;
+            }
+        } else {
+            return (ULONG_PTR)((BYTE*)curMod + foundRVA);
+        }
     }
-    buf[i] = 0;
-
-    ustr->Length = (USHORT)(i * sizeof(wchar_t));
-    ustr->MaximumLength = (USHORT)((i + 1) * sizeof(wchar_t));
-    ustr->Buffer = buf;
 }
 
-static void InitUnicodeString(UNICODE_STRING* ustr, const wchar_t* wstr) {
-    size_t len = 0;
-    while (wstr[len]) len++;
+static void ResolveImport(BYTE* base, IMAGE_DATA_DIRECTORY im) {
+    IMAGE_IMPORT_DESCRIPTOR* imp = (IMAGE_IMPORT_DESCRIPTOR*)(base + im.VirtualAddress);
+    while (imp->Name) {
+        char* name = (char*)(base + imp->Name);
+        HMODULE mod = myLoadLibraryA(name);
+        IMAGE_THUNK_DATA64* orig = (IMAGE_THUNK_DATA64*)(base + imp->OriginalFirstThunk);
+        IMAGE_THUNK_DATA64* addr = (IMAGE_THUNK_DATA64*)(base + imp->FirstThunk);
 
-    ustr->Buffer        = (PWSTR)wstr;
-    ustr->Length        = (USHORT)(len * sizeof(WCHAR));
-    ustr->MaximumLength = (USHORT)((len + 1) * sizeof(WCHAR));
+        for (; orig->u1.AddressOfData; orig++, addr++) {
+            const char* funcName;
+            int isOrdinal = 0;
+
+            if (orig->u1.AddressOfData & IMAGE_ORDINAL_FLAG64) {
+                funcName = (const char*)(ULONG_PTR)IMAGE_ORDINAL64(orig->u1.Ordinal);
+                isOrdinal = 1;
+            } else funcName = ((IMAGE_IMPORT_BY_NAME*)(base + orig->u1.AddressOfData))->Name;
+
+            addr->u1.Function = ResolveExport(mod, funcName, isOrdinal);
+        }
+        imp++;
+    }
 }
 
-// -------------------- myGetModuleHandleA --------------------
-static HMODULE myGetModuleHandleA(const char* name) {
-    PEB* peb = (PEB*)__readgsqword(0x60);
-    LIST_ENTRY* head = &peb->Ldr->InMemoryOrderModuleList;
+// -------------------- PE mapping --------------------
+static void* MapImage(unsigned char* data) {
+    HMODULE ntdll = myGetModuleHandleA(ntdll_dll);
+    NtAllocateVirtualMemory_t NtAllocateVirtualMemory = (NtAllocateVirtualMemory_t)myGetProcAddress(ntdll, ntallocatevirtualmemory);
+    NtProtectVirtualMemory_t NtProtectVirtualMemory = (NtProtectVirtualMemory_t)myGetProcAddress(ntdll, ntprotectvirtualmemory);
+    NtFreeVirtualMemory_t NtFreeVirtualMemory = (NtFreeVirtualMemory_t)myGetProcAddress(ntdll, ntfreevirtualmemory);
 
-    for (LIST_ENTRY* cur = head->Flink; cur != head; cur = cur->Flink) {
-        PLDR_MODULE mod = (PLDR_MODULE)((BYTE*)cur - offsetof(LDR_MODULE, InMemoryOrderModuleList));
-        SIZE_T len = mod->BaseDllName.Length / sizeof(WCHAR);
-        SIZE_T i;
-        for (i = 0; i < len && ((char)(mod->BaseDllName.Buffer[i]) | 0x20) == (name[i] | 0x20); ++i);
-        if (i == len && name[i] == 0)
-            return (HMODULE)mod->BaseAddress;
+    IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)data;
+    IMAGE_NT_HEADERS64* nt = (IMAGE_NT_HEADERS64*)(data + dos->e_lfanew);
+    IMAGE_SECTION_HEADER* sec = IMAGE_FIRST_SECTION(nt);
+
+    PVOID base = NULL;
+    SIZE_T totalSize = nt->OptionalHeader.SizeOfImage;
+    NtAllocateVirtualMemory((HANDLE)-1, &base, 0, &totalSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+    // Copy headers
+    for (SIZE_T i = 0; i < nt->OptionalHeader.SizeOfHeaders; i++)
+        ((BYTE*)base)[i] = data[i];
+
+    // Copy sections
+    for (WORD i = 0; i < nt->FileHeader.NumberOfSections; i++) {
+        BYTE* dest = (BYTE*)base + sec[i].VirtualAddress;
+        BYTE* src  = data + sec[i].PointerToRawData;
+        for (DWORD j = 0; j < sec[i].SizeOfRawData; j++)
+            dest[j] = src[j];
     }
 
-    return NULL;
-}
-
-// -------------------- myGetProcAddress --------------------
-static FARPROC myGetProcAddress(HMODULE hMod, const char* fnName) {
-    BYTE* base = (BYTE*)hMod;
-    IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)base;
-    IMAGE_NT_HEADERS64* nt = (IMAGE_NT_HEADERS64*)(base + dos->e_lfanew);
-
-    IMAGE_EXPORT_DIRECTORY* exp = (IMAGE_EXPORT_DIRECTORY*)
-        (base + nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
-
-    DWORD* names = (DWORD*)(base + exp->AddressOfNames);
-    WORD* ords  = (WORD*)(base + exp->AddressOfNameOrdinals);
-    DWORD* funcs = (DWORD*)(base + exp->AddressOfFunctions);
-
-    for (DWORD i = 0; i < exp->NumberOfNames; i++) {
-        const char* p1 = (const char*)(base + names[i]);
-        const char* p2 = fnName;
-        while (*p1 && (*p1) == (*p2)) { ++p1; ++p2; }
-        if (*p2 == 0)
-            return (FARPROC)(base + funcs[ords[i]]);
+    // Apply relocations
+    ULONG_PTR delta = (ULONG_PTR)base - nt->OptionalHeader.ImageBase;
+    IMAGE_DATA_DIRECTORY rl = nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+    if (rl.Size) {
+        IMAGE_BASE_RELOCATION* r = (IMAGE_BASE_RELOCATION*)((BYTE*)base + rl.VirtualAddress);
+        BYTE* end = (BYTE*)r + rl.Size;
+        while ((BYTE*)r < end && r->SizeOfBlock) {
+            WORD* list = (WORD*)(r + 1);
+            DWORD count = (r->SizeOfBlock - sizeof(*r)) / sizeof(WORD);
+            for (DWORD i = 0; i < count; i++) {
+                if ((list[i] >> 12) == IMAGE_REL_BASED_DIR64) {
+                    ULONG_PTR* ptr = (ULONG_PTR*)((BYTE*)base + r->VirtualAddress + (list[i] & 0xFFF));
+                    *ptr += delta;
+                }
+            }
+            r = (IMAGE_BASE_RELOCATION*)((BYTE*)r + r->SizeOfBlock);
+        }
     }
-    return NULL;
+
+    // Imports
+    ResolveImport((BYTE*)base, nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT]);
+
+    // Protect headers + first section
+    BYTE* regionStart = (BYTE*)base;
+    SIZE_T regionSize = sec[0].VirtualAddress + sec[0].Misc.VirtualSize;
+    ULONG currentProt = SectionProtection(sec[0].Characteristics);
+    ULONG oldProt;
+
+    // Protect remaining sections
+    for (WORD i = 1; i < nt->FileHeader.NumberOfSections; i++, sec++) {
+        ULONG secProt = SectionProtection(sec[i].Characteristics);
+        BYTE* secStart = (BYTE*)base + sec[i].VirtualAddress;
+        SIZE_T secSize = sec[i].Misc.VirtualSize;
+
+        if (secProt == currentProt && regionStart + regionSize == secStart)
+            regionSize += secSize; // Merge with previous
+        else {
+            NtProtectVirtualMemory((HANDLE)-1, (PVOID*)&regionStart, &regionSize, currentProt, &oldProt);
+            regionStart = secStart;
+            regionSize = secSize;
+            currentProt = secProt;
+        }
+    }
+
+    if (regionSize)
+        NtProtectVirtualMemory((HANDLE)-1, (PVOID*)&regionStart, &regionSize, currentProt, &oldProt);
+
+    // Free discardable sections
+    sec = IMAGE_FIRST_SECTION(nt);
+    for (WORD i = 0; i < nt->FileHeader.NumberOfSections; i++, sec++) {
+        if (sec[i].Characteristics & IMAGE_SCN_MEM_DISCARDABLE) {
+            PVOID discardBase = (BYTE*)base + sec[i].VirtualAddress;
+            SIZE_T discardSize = sec[i].Misc.VirtualSize;
+            NtFreeVirtualMemory((HANDLE)-1, &discardBase, &discardSize, MEM_DECOMMIT);
+        }
+    }
+
+    return base;
 }
 
-// -------------------- myLoadLibrary helper --------------------
-typedef NTSTATUS(NTAPI* LdrLoadDll_t)(
-    PWSTR PathToFile,
-    ULONG Flags,
-    UNICODE_STRING* ModuleFileName,
-    PHANDLE ModuleHandle
-);
+// -------------------- Execute entry --------------------
+static void ExecuteFromMemory(unsigned char* data) {
+    BYTE* image = MapImage(data);
+    IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)image;
+    IMAGE_NT_HEADERS64* nt = (IMAGE_NT_HEADERS64*)(image + dos->e_lfanew);
 
-static HMODULE _myLdrLoadDll(UNICODE_STRING* ustr) {
-    char stackbuf[21];
-
-    char* ntdll_dll = &stackbuf[0]; // 10 bytes
-	char* ldrloaddll = &stackbuf[10]; // 11 bytes
-#ifdef XOR
-    ntdll_dll[0] = 'n'^XOR_KEY(9); ntdll_dll[1] = 't'^XOR_KEY(9);
-    ntdll_dll[2] = 'd'^XOR_KEY(9); ntdll_dll[3] = 'l'^XOR_KEY(9);
-    ntdll_dll[4] = 'l'^XOR_KEY(9); ntdll_dll[5] = '.'^XOR_KEY(9);
-    ntdll_dll[6] = 'd'^XOR_KEY(9); ntdll_dll[7] = 'l'^XOR_KEY(9);
-    ntdll_dll[8] = 'l'^XOR_KEY(9); ntdll_dll[9] = 0;
-    xor_decode(ntdll_dll);
-	
-	ldrloaddll[0] = 'L'^XOR_KEY(10); ldrloaddll[1] = 'd'^XOR_KEY(10);
-    ldrloaddll[2] = 'r'^XOR_KEY(10); ldrloaddll[3] = 'L'^XOR_KEY(10);
-    ldrloaddll[4] = 'o'^XOR_KEY(10); ldrloaddll[5] = 'a'^XOR_KEY(10);
-    ldrloaddll[6] = 'd'^XOR_KEY(10); ldrloaddll[7] = 'D'^XOR_KEY(10);
-    ldrloaddll[8] = 'l'^XOR_KEY(10); ldrloaddll[9] = 'l'^XOR_KEY(10);
-    ldrloaddll[10] = 0;
-    xor_decode(ldrloaddll);
-#else
-    ntdll_dll[0] = 'n'; ntdll_dll[1] = 't'; ntdll_dll[2] = 'd';
-    ntdll_dll[3] = 'l'; ntdll_dll[4] = 'l'; ntdll_dll[5] = '.';
-    ntdll_dll[6] = 'd'; ntdll_dll[7] = 'l'; ntdll_dll[8] = 'l';
-    ntdll_dll[9] = 0;
-	
-    ldrloaddll[0] = 'L'; ldrloaddll[1] = 'd'; ldrloaddll[2] = 'r';
-    ldrloaddll[3] = 'L'; ldrloaddll[4] = 'o'; ldrloaddll[5] = 'a';
-    ldrloaddll[6] = 'd'; ldrloaddll[7] = 'D'; ldrloaddll[8] = 'l';
-    ldrloaddll[9] = 'l'; ldrloaddll[10] = 0;
-#endif
-
-    HMODULE hNtdll = myGetModuleHandleA(ntdll_dll);
-    LdrLoadDll_t pLdrLoadDll = (LdrLoadDll_t)myGetProcAddress(hNtdll, ldrloaddll);
-
-    HMODULE hModule = NULL;
-    pLdrLoadDll(NULL, 0, ustr, (PHANDLE)&hModule);
-
-    return hModule;
+    void (*entry)(void) = (void(*)(void))(image + nt->OptionalHeader.AddressOfEntryPoint);
+    entry();
 }
 
-// -------------------- myLoadLibraryA --------------------
-static HMODULE myLoadLibraryA(const char* dllNameA) {
-    size_t len = 0;
-    while (dllNameA[len]) len++;
-    wchar_t buf[len + 1];
-
-    UNICODE_STRING ustr;
-    AsciiToWideChar(dllNameA, &ustr, buf, len + 1);
-    return _myLdrLoadDll(&ustr);
-}
-
-// -------------------- myLoadLibraryW --------------------
-static HMODULE myLoadLibraryW(const wchar_t* dllNameW) {
-    UNICODE_STRING ustr;
-    InitUnicodeString(&ustr, dllNameW);
-    return _myLdrLoadDll(&ustr);
-}
-
-#endif // WINAPI_LOADER_H
+#endif // PE_LOADER_H
