12397eb0d8ff28274d7bc48e09ef31c91f99f996
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2026-01-06 01:46:44 -0500

    Update example.c

--- loader/example.c
+++ loader/example.c
@@ -3,8 +3,6 @@
 __attribute__((section(".text")))
 unsigned char pe_blob[] = { /* x64 native PE bytes go here */ };
 
-unsigned int pe_blob_len = sizeof(pe_blob);
-
 __attribute__((section(".text.start")))
 void _start(void) {
     ExecuteFromMemory(pe_blob);
