3b904c1e19bf6850a6311d38b859ac16a1e236b2
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2026-01-10 11:11:58 -0500

    Delete loader/example.c

--- loader/example.c
+++
@@ -1,9 +0,0 @@
-#include "pe_loader.h"
-
-__attribute__((section(".text")))
-unsigned char pe_blob[] = { /* x64 native PE bytes go here */ };
-
-__attribute__((section(".text.start")))
-void _start(void) {
-    ExecuteFromMemory(pe_blob);
-}
